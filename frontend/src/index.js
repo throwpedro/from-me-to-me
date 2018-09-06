47c2b1dbed3ea5f9a8d9cb1f756f755722128c5e
Thor <thordoor89@hotmail.com>
2018-09-06 20:30:25 +0200

    Removed everything and should try without react-create-app. Instead I will add react manually and webpack and have more control.

--- frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Input from './textinput/input';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Input />, document.getElementById('root'));
-registerServiceWorker();
